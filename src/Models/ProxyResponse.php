007da3a97f15979488d0d8f424f55d470c056e85
Luis José <ruaperjose@gmail.com>
2018-04-17 12:30:01 -0600

    fix auth and repo

--- src/Models/ProxyResponse.php
+++ src/Models/ProxyResponse.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @package   shokmaster/laravel-proxify
+ * @package   CianCoders/laravel-proxify
  * @author    Michele Andreoli <michi.andreoli[at]gmail.com>
  * @copyright Copyright (c) Michele Andreoli
  * @author    Rik Schreurs <rik.schreurs[at]mail.com>
@@ -10,8 +10,10 @@
  * @copyright Copyright (c) Siddhant Baviskar
  * @author    Siddhant Baviskar <shokmaster[at]gmail.com>
  * @copyright Copyright (c) Juan Antonio Gómez Benito
+ * @author    Luis José Ruano Pérez <rp.luisjose[at]gmail.com>
+ * @copyright Copyright (c) Luis Ruano
  * @license   http://mit-license.org/
- * @link      https://github.com/shokmaster/laravel-proxify
+ * @link      https://github.com/CianCoders/laravel-proxify
  */
 
 namespace Shokmaster\LaravelProxify\Models;
